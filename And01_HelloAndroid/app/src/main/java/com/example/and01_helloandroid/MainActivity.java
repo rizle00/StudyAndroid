d229c7523a3851594dda8b5cdf7e1133c10b3c40
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-11-13 13:33:54 +0900

    And

---
+++ And01_HelloAndroid/app/src/main/java/com/example/and01_helloandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.and01_helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = new String();// ctrl+p
+        //코드 자동 완성 : alr + insert, 마우스 오른쪽 클릭 -> 자동완성(Generate)
+
+
+
+
+
+
+
+
+    }
+}
